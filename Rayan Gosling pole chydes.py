0fac3db38f94cd0d4b76494be003a6c72a97d5f0
Makar2806 <165052309+Makar2806@users.noreply.github.com>
2024-04-02 18:09:10 +0300

    Update Rayan Gosling pole chydes.py
    Объединение всех файлов

--- Rayan Gosling pole chydes.py
+++ Rayan Gosling pole chydes.py
@@ -1,6 +1,6 @@
 
 import telebot
-
+from telebot import types
 
 bot = telebot.TeleBot("6736959406:AAFwdUBqmBybnmoEZKzB3eau0v2dXO3cOwo")
 
@@ -12,6 +12,21 @@ def test(m):
 @bot.message_handler(commands=["hello"])
 def test(m):
     bot.send_message(m.chat.id,"Приветствую вас!")
+
+
+def choose (chatid):
+    kb = types.ReplyKeyboardMarkup()
+    kb.add(types.KeyboardButton('Крутить барабан'))
+    kb.add(types.KeyboardButton('Назвать слово'))
+    kb.add(types.KeyboardButton('Назвать букву'))
+    bot.send_message(chatid, "Выбирете действие:", reply_markup=kb)
+
+def proverka(players, game):
+    if len(players) > 3:
+        if not game["srart"]:
+            return True
+        return  False
+    return False
 
 
 
