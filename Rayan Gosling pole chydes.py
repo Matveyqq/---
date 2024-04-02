bb8593d32ac5cadabdaff1ebfdff6c7b3e57b4a2
Matveyqq <155990700+Matveyqq@users.noreply.github.com>
2024-04-02 19:30:08 +0300

    Add files via upload

--- Rayan Gosling pole chydes.py
+++ Rayan Gosling pole chydes.py
@@ -1,12 +1,16 @@
-
 import telebot
 from telebot import types
+games = {}
 
 bot = telebot.TeleBot("6736959406:AAFwdUBqmBybnmoEZKzB3eau0v2dXO3cOwo")
 
 @bot.message_handler(commands=["start"])
 def test(m):
-    bot.send_message(m.chat.id,"Игра началась!")
+    if proverka() == True:
+     bot.send_message(m.chat.id,"Игра началась!")
+    else:
+        bot.send_message(m.chat.id, "Игра еще не началась!")
+
 
 
 @bot.message_handler(commands=["hello"])
